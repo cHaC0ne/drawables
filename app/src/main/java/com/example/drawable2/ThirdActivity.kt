25c8bed1159d3da7395a9bbf1a8f038df8bb2c23
Alumno <asd@asd.com>
2022-07-06 19:17:44 +0200

    seek bar

--- app/src/main/java/com/example/drawable2/ThirdActivity.kt
+++ app/src/main/java/com/example/drawable2/ThirdActivity.kt
@@ -13,37 +13,21 @@ class ThirdActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         b = ActivityThirdBinding.inflate(layoutInflater)
         setContentView(b.root)
+//        b.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+//            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+//                b.tvSeekBar.text = seek.progress.toString().plus("/100")
+//
+//            }
+//
+//            override fun onStartTrackingTouch(p0: SeekBar?) {
+//                TODO("Not yet implemented")
+//            }
+//
+//            override fun onStopTrackingTouch(p0: SeekBar?) {
+//                TODO("Not yet implemented")
+//            }
+//        })
 
-        b.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-
-            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
-                b.tvSeekBar.text = seek.progress.toString().plus("/100")
-
-            }
-
-            override fun onStartTrackingTouch(seek: SeekBar) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onStopTrackingTouch(seek: SeekBar) {
-                TODO("Not yet implemented")
-            }
-        })
-        b.seekBar2 .setOnSeekBarChangeListener (object  : SeekBar.OnSeekBarChangeListener {
-
-            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
-                b.tvSeekBar.text = seek.progress.toString().plus("/100")
-
-            }
-
-            override fun onStartTrackingTouch(seek: SeekBar?) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onStopTrackingTouch(seek: SeekBar?) {
-                TODO("Not yet implemented")
-            }
-        })
     }
 
 }
