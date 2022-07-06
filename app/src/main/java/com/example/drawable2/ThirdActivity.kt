ec02005b816392fcd64ba0a09e1f2542818b2ede
Alumno <asd@asd.com>
2022-07-06 18:56:07 +0200

    seek bar

--- app/src/main/java/com/example/drawable2/ThirdActivity.kt
+++ app/src/main/java/com/example/drawable2/ThirdActivity.kt
@@ -2,10 +2,48 @@ package com.example.drawable2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import com.example.drawable2.databinding.ActivityMainBinding
+import com.example.drawable2.databinding.ActivityThirdBinding
 
 class ThirdActivity : AppCompatActivity() {
+    private lateinit var b: ActivityThirdBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_third)
+        b = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(b.root)
+
+        b.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                b.tvSeekBar.text = seek.progress.toString().plus("/100")
+
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+                TODO("Not yet implemented")
+            }
+        })
+        b.seekBar2 .setOnSeekBarChangeListener (object  : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                b.tvSeekBar.text = seek.progress.toString().plus("/100")
+
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+        })
     }
+
 }
