0f84e395ad39969d1779b494ca627c0faa6284cd
Alumno <asd@asd.com>
2022-07-06 20:25:00 +0200

    seek bar

--- app/src/main/java/com/example/drawable2/MainActivity.kt
+++ app/src/main/java/com/example/drawable2/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.drawable2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.drawable2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,24 +19,29 @@ class MainActivity : AppCompatActivity() {
 
 
         b.bNavView.setOnItemSelectedListener { item ->
-            when (item.itemId){
+            when (item.itemId) {
                 R.id.bInicio -> {
-                    Navigation.findNavController(b.fragmentContainerView2  )
+//                    supportFragmentManager.findFragmentById(androidx.navigation.fragment.R.id.nav_host_fragment_container) as  NavHostFragment
+//                    val navController = NavHostFragment.findNavController(n)
+                    Navigation.findNavController(b.fragmentContainerView2)
                         .navigate(R.id.fristFrag)
                     true
+
                 }
                 R.id.bProfile -> {
-                    Navigation.findNavController(b.fragmentContainerView2  )
-                        .navigate(R.id.secondFrag)
+                    Navigation.findNavController(b.fragmentContainerView2)
+                        .navigate(R.id.thirdFrag)
                     true
                 }
                 R.id.bSetting -> {
-                    Navigation.findNavController(b.fragmentContainerView2  )
-                        .navigate(R.id.thirdFrag)
+                    Navigation.findNavController(b.fragmentContainerView2)
+                        .navigate(R.id.secondFrag)
                     true
                 }
                 else -> false
             }
         }
+
     }
 }
+
